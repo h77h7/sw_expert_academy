ab4a99aef504e6ccb9207eca3d8ddfa63552b4f5
h77h7 <74125523+h77h7@users.noreply.github.com>
2021-04-01 17:57:20 +0900

    길찾기(수정)

--- s1219.cpp
+++ s1219.cpp
@@ -3,43 +3,43 @@
 
 using namespace std;
 
-bool move(int cur,int* road1,int* road2) {
+int road1[100] = { 0, };
+int road2[100] = { 0, };
+bool move(int cur) {
 	if (cur == 99) return true;
-	
-	//다음 위치 찾고 길 없애기
-	int a = road1[cur];
-	int b = road2[cur];
-	road1[cur] = -1;
-	road2[cur] = -1;
-
-	if (a <= 0 && b <= 0) return false;
-	else if (a > 0 && b > 0) return move(a, road1, road2) | move(b, road1, road2);
-	else if (a > 0) return move(a, road1, road2);
-	else if (b > 0) return move(b, road1, road2);
-
+	int res = false;
+	if (road1[cur]) {
+		res = move(road1[cur]);
+		if (res) return res;
+	}
+	if (road2[cur]) {
+		res = move(road2[cur]);
+		if (res) return res;
+	}
+	return false;
 }
 
 int main() {
-	//freopen("input.txt", "r", stdin);
+	freopen("input.txt", "r", stdin);
 	setbuf(stdout, NULL);
 
 	for (int t = 1; t <= 10; t++)
 	{
 		int tc = 0, N = 0;
-		scanf("%d %d", &tc, &N);
+		scanf(" %d %d" , &tc, &N);
 
-		int road1[100] = { 0, };
-		int road2[100] = { 0, };
+		//int road1[100] = { 0, };
+		//int road2[100] = { 0, };
 
 		for (int i = 0; i < N; i++) {
 			int a = 0, b = 0;
-			scanf(" %d %d", &a, &b);
+			scanf(" %d %d" , &a, &b);
 			if (road1[a] > 0) road2[a] = b;
 			else road1[a] = b;
 		}
 
-		bool result = move(0, road1, road2);
-		printf("#%d %d\n", t, (result ? 1 : 0));
+		bool result = move(0);
+		printf(" #%d %d\n" , t, (result ? 1 : 0));
 	}
 	return 0;
 }
