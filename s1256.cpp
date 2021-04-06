2e554ce23859d81d851a31b677b2ec0a423c2b81
h77h7 <74125523+h77h7@users.noreply.github.com>
2021-04-06 09:25:29 +0900

    quick sort -> selection

--- s1256.cpp
+++ s1256.cpp
@@ -3,7 +3,6 @@
 
 using namespace std;
 
-#define SWAP(x,y) {x = x^y; y = y^x; x = x^y;}
 const int MAX = 400;
 int arrLen = 0;
 char arr[MAX] = { 0, };
@@ -16,14 +15,32 @@ int str_cmp(int idx1, int idx2) {
 	return (arr[idx1] - arr[idx2]);
 }
 
+//카운팅 정렬도 사용가능
+//suffix array를 카운팅 정렬로 정렬
+
+//K번째 까지만 찾으면 되니까 selection 
+void selection(int* a, int K, int arrLen) {
+	for (int i = 0; i < K; i++) {
+		int minIdx = i;
+		for (int j = i + 1; j < arrLen; j++) {
+			if (str_cmp(a[minIdx], a[j]) > 0) {
+				minIdx = j;
+			}
+		}
+		int tmp = a[i];
+		a[i] = a[minIdx];
+		a[minIdx] = tmp;
+	}
+}
+
 void quicksort(int* a, int begin, int end) {
 	if (begin < end) {
 		int pivot = a[begin];
-		int i = begin;
+		int i = begin+1;
 		int j = end;
 		while (i <= j) {
-			while (i <= end && str_cmp(a[i],pivot) <= 0) i++;
-			while (j >= begin && str_cmp(a[j],pivot) > 0) j--;
+			while (i <= end && str_cmp(a[i], pivot) <= 0) i++;
+			while (j >= begin && str_cmp(a[j], pivot) > 0) j--;
 
 			if (i < j) {
 				swap(a[i], a[j]);
@@ -37,10 +54,9 @@ void quicksort(int* a, int begin, int end) {
 }
 
 int main() {
-	freopen("input_1256.txt", "r", stdin);
+	//freopen("input_1256.txt", "r", stdin);
 
 	cin.tie(NULL);
-	//ios::sync_with_stdio(false);
 
 	int tc;
 	cin >> tc;
@@ -48,7 +64,7 @@ int main() {
 	for (int t = 1; t <= tc; t++) {
 		int K;
 		cin >> K;
-		
+
 		scanf(" %s", arr);
 		int A[400] = { 0, };
 		int arrIdx = 0;
@@ -59,12 +75,19 @@ int main() {
 		}
 		arrLen = arrIdx;
 
-		quicksort(A, 0, arrIdx-1);
+		if (K < arrLen) {
+			//quicksort(A, 0, arrIdx - 1);
+			selection(A, K, arrLen);
 
-		cout << "#" << t << " ";
-		for (int i = A[K-1]; i < arrLen; i++) {
-			cout << arr[i];
+			cout << "#" << t << " ";
+			for (int i = A[K - 1]; i < arrLen; i++) {
+				cout << arr[i];
+			}
+			cout << endl;
 		}
-		cout << endl;
+		else {
+			cout << "none\n";
+		}
+		
 	}
 }
